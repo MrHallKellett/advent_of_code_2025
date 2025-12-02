6be6956682204aec1ae41e6de6e22ff39e2f66fd
MrHallKellett <chall@kellettschool.com>
2025-12-02 12:59:30 +0800

    day2

---
+++ day2a.py
@@ -0,0 +1,46 @@
+from re import search, match, findall
+from collections import Counter, defaultdict
+from itertools import product, combinations, permutations
+from math import inf as INF
+from math import prod
+from helpers import *
+
+PP_ARGS = False, False #rotate, cast int
+
+DAY = 2
+TEST_DELIM = "---"
+FILE_DELIM = "\n"
+TESTS = """a,a///aa
+"""
+
+DEBUG = True
+
+
+def solve(data):
+    count = 0
+
+    for row in data:
+        pass
+        
+
+        
+    
+
+    return count
+
+
+
+
+if __name__ == "__main__":
+    p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
+
+    try:
+        if p.check(TESTS, solve):
+            puzzle_input = p.load_puzzle()
+            puzzle_input = p.pre_process(puzzle_input, *PP_ARGS)
+            print("FINAL ANSWER: ", solve(puzzle_input))
+    except Exception as e:
+        p.log.close()
+        raise e
+
+        
